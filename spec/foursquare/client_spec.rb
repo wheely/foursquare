d7111db32e5645e89e5ad0882ae506d307b83ff0
Ivan Samsonov <ivan@wheely.com>
2011-08-16 17:28:56 +0400

    Do not ignore additonal options

--- spec/foursquare/client_spec.rb
+++ spec/foursquare/client_spec.rb
@@ -6,16 +6,28 @@ describe Foursquare::Client do
     @client = Foursquare.new(ENV['FOURSQUARE_ID'],ENV['FOURSQUARE_SECRET'])
   end
   
-  it "should perform a search request" do
+  it "performs a search request" do
     result = nil
-    EM.run {
+    EM.run do
       @client.callback do |locations|
         result = locations
         EM.stop
       end
       @client.search('48.218538,16.359950')
-    }
-    result.class.should == Array
+    end
+    result.size.should > 0
   end
-  
+
+  it "accepts additional options" do
+    result = nil
+    EM.run do
+      @client.callback do |locations|
+        result = locations
+        EM.stop
+      end
+      @client.search('38.89761038124561,-77.03672021627426', :query => 'White House')
+    end
+
+    result.size.should > 0
+  end
 end
